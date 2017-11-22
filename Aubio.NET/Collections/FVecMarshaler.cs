9e941adda9ef41d6f0c104bc0914111bfd1201d3
Aybe <aybe@users.noreply.github.com>
2017-11-22 04:23:21 +0100

    Sealing this class

--- Aubio.NET/Collections/FVecMarshaler.cs
+++ Aubio.NET/Collections/FVecMarshaler.cs
@@ -4,7 +4,7 @@ using JetBrains.Annotations;
 
 namespace Aubio.NET.Collections
 {
-    internal   class FVecMarshaler : ICustomMarshaler
+    internal sealed class FVecMarshaler : ICustomMarshaler
     {
         private static readonly FVecMarshaler Instance = new FVecMarshaler();
 
