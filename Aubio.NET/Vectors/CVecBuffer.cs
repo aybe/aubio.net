7b21054f215ee7dca57a02e5c55952be38aa2f0f
Aybe <aybe@users.noreply.github.com>
2017-11-24 22:07:24 +0100

    CVecBuffer now implements IVector directly

--- Aubio.NET/Vectors/CVecBuffer.cs
+++ Aubio.NET/Vectors/CVecBuffer.cs
@@ -1,13 +1,14 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using Aubio.NET.Vectors.Native;
 using JetBrains.Annotations;
 
 namespace Aubio.NET.Vectors
 {
-    public abstract class CVecBuffer : VectorUnmanagedFloat
+    public abstract class CVecBuffer : IVector<float>
     {
         internal unsafe CVecBuffer([NotNull] CVec cVec, [NotNull] float* data, int length)
-            : base(data, length)
         {
             if (data == null)
                 throw new ArgumentNullException(nameof(data));
@@ -16,6 +17,26 @@ namespace Aubio.NET.Vectors
         }
 
         protected CVec CVec { get; }
+
+        public abstract float this[int index] { get; set; }
+
+        public int Length => CVec.Length;
+
+        public IEnumerator<float> GetEnumerator()
+        {
+            return new VectorEnumerator<float>(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        protected void ThrowOnInvalidIndex(int index)
+        {
+            if (index < 0 || index >= Length)
+                throw new IndexOutOfRangeException();
+        }
 
         public abstract unsafe float* GetData();
 
