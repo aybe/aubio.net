9717ca48407acc17727f229ad1e2fd1f6dfc2798
Aybe <aybe@users.noreply.github.com>
2017-11-24 07:50:36 +0100

    Added a generic marshaler that'll centralize all the checks !

---
+++ Aubio.NET/AubioObjectMarshaler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using JetBrains.Annotations;
+
+namespace Aubio.NET
+{
+    internal sealed class AubioObjectMarshaler : ICustomMarshaler
+    {
+        private static readonly AubioObjectMarshaler Instance = new AubioObjectMarshaler();
+
+        private AubioObjectMarshaler()
+        {
+        }
+
+        public void CleanUpManagedData(object managedObj)
+        {
+        }
+
+        public void CleanUpNativeData(IntPtr pNativeData)
+        {
+        }
+
+        public int GetNativeDataSize()
+        {
+            throw new NotImplementedException(); // not needed
+        }
+
+        public IntPtr MarshalManagedToNative([NotNull] object managedObj)
+        {
+            if (managedObj == null)
+                throw new ArgumentNullException(nameof(managedObj));
+
+            if (!(managedObj is AubioObject aubioObject))
+                throw new ArgumentNullException(nameof(aubioObject));
+
+            if (aubioObject.IsDisposed)
+                throw new ObjectDisposedException(nameof(aubioObject));
+
+            var pointer = aubioObject.ToPointer();
+
+            return pointer;
+        }
+
+        public object MarshalNativeToManaged(IntPtr pNativeData)
+        {
+            throw new NotImplementedException();
+        }
+
+        [UsedImplicitly]
+        public static ICustomMarshaler GetInstance(string pstrCookie)
+        {
+            return Instance;
+        }
+    }
+}
