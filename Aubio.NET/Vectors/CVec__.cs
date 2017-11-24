2284378d30cca11576090fe3194420b44a6b254d
Aybe <aybe@users.noreply.github.com>
2017-11-24 22:45:58 +0100

    Fixed some pragmas

--- Aubio.NET/Vectors/CVec__.cs
+++ Aubio.NET/Vectors/CVec__.cs
@@ -6,8 +6,8 @@ namespace Aubio.NET.Vectors
     internal struct CVec__
     {
 #pragma warning disable 649
-#pragma warning disable 169
         public readonly uint Length;
+#pragma warning disable 169
         public readonly unsafe float* Norm;
         public readonly unsafe float* Phas;
 #pragma warning restore 169
--- Aubio.NET/Vectors/FVec__.cs
+++ Aubio.NET/Vectors/FVec__.cs
@@ -7,7 +7,9 @@ namespace Aubio.NET.Vectors
     {
 #pragma warning disable 649
         public readonly uint Length;
+#pragma warning disable 169
         public readonly float* Data;
+#pragma warning restore 169
 #pragma warning restore 649
     }
 }
