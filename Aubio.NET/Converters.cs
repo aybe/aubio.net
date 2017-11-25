8625b2fca0cfa72f0d7223350961f23a70748a02
Aybe <aybe@users.noreply.github.com>
2017-11-25 05:30:39 +0100

    Added extra converters

--- Aubio.NET/Converters.cs
+++ Aubio.NET/Converters.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Security;
 using JetBrains.Annotations;
 
 namespace Aubio.NET
@@ -42,5 +44,90 @@ namespace Aubio.NET
             var samples = MillisecondsToSamples(sampleRate, milliseconds);
             return samples;
         }
+
+        public static float BinToMidi(float bin, float sampleRate, float fftSize)
+        {
+            return aubio_bintomidi(bin, sampleRate, fftSize);
+        }
+
+        public static float MidiToBin(float midi, float sampleRate, float fftSize)
+        {
+            return aubio_miditobin(midi, sampleRate, fftSize);
+        }
+
+        public static float BinToFreq(float bin, float sampleRate, float fftSize)
+        {
+            return aubio_bintofreq(bin, sampleRate, fftSize);
+        }
+
+        public static float FreqToBin(float freq, float sampleRate, float fftSize)
+        {
+            return aubio_freqtobin(freq, sampleRate, fftSize);
+        }
+
+        public static float FreqToMidi(float freq)
+        {
+            return aubio_freqtomidi(freq);
+        }
+
+        public static float MidiToFreq(float midi)
+        {
+            return aubio_miditofreq(midi);
+        }
+
+        public static float Unwrap2Pi(float phase)
+        {
+            return aubio_unwrap2pi(phase);
+        }
+
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport("aubio")]
+        private static extern float aubio_unwrap2pi(
+            float phase
+        );
+
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport("aubio")]
+        private static extern float aubio_bintomidi(
+            float bin,
+            float sampleRate,
+            float fftSize
+        );
+
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport("aubio")]
+        private static extern float aubio_miditobin(
+            float midi,
+            float sampleRate,
+            float fftSize
+        );
+
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport("aubio")]
+        private static extern float aubio_bintofreq(
+            float bin,
+            float sampleRate,
+            float fftSize
+        );
+
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport("aubio")]
+        private static extern float aubio_freqtobin(
+            float freq,
+            float sampleRate,
+            float fftSize
+        );
+
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport("aubio")]
+        private static extern float aubio_freqtomidi(
+            float freq
+        );
+
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport("aubio")]
+        private static extern float aubio_miditofreq(
+            float midi
+        );
     }
 }
