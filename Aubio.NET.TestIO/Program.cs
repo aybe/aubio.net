a0dc5f4cbd1680ecfbc675b9992e6c9136708821
Aybe <aybe@users.noreply.github.com>
2017-11-26 03:09:41 +0100

    Added Log test program

--- Aubio.NET.TestIO/Program.cs
+++ Aubio.NET.TestIO/Program.cs
@@ -11,13 +11,17 @@ namespace Aubio.NET.TestIO
         {
             var uri = args[0];
 
+            //uri = @"C:\Users\Aybe\Music\1m.wav";
+
             if (!File.Exists(uri))
                 throw new FileNotFoundException();
+
+            var path = Path.ChangeExtension(uri, $".sink.{Path.GetExtension(uri)}");
 
             const int hopSize = 1024;
 
             using (var source = new Source(uri, 0, hopSize))
-            using (var sink = new Sink("test.wav"))
+            using (var sink = new Sink(path))
             using (var buffer = new FMat(source.Channels, hopSize))
             {
                 // TODO this sink does not resample !
@@ -30,17 +34,42 @@ namespace Aubio.NET.TestIO
 
                 var totalRead = 0;
                 int framesRead;
-
+                int blocks = 0;
                 do
                 {
                     source.DoMulti(buffer, out framesRead);
+
                     sink.DoMulti(buffer, framesRead);
 
                     totalRead += framesRead;
+                    blocks++;
 
-                    Console.WriteLine($"{(float) totalRead / source.Duration.Samples:P}");
+                    var percent = (float)totalRead / source.Duration.Samples;
+                    PrintProgress(percent, 0, 0, 25);
                 } while (framesRead == hopSize);
             }
+        }
+
+        private static void PrintProgress(float percent, int x, int y, int width)
+        {
+            if (percent < 0.0f || percent > 1.0f)
+                throw new ArgumentOutOfRangeException(nameof(percent));
+
+            if (x < 0)
+                throw new ArgumentOutOfRangeException(nameof(x));
+
+            if (y < 0)
+                throw new ArgumentOutOfRangeException(nameof(y));
+
+            if (width <= 0)
+                throw new ArgumentOutOfRangeException(nameof(width));
+
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(x, y);
+            var i = (int)(width * percent);
+            var s1 = new string('█', i);
+            var s2 = new string('▒', width - i);
+            Console.Write($"{s1}{s2} {percent:P}");
         }
     }
 }
---
+++ Aubio.NET.TestLog/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Aubio.NET.Detection;
+using Aubio.NET.Vectors;
+
+namespace Aubio.NET.TestLog
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            // well, it is not thoroughly applied in sources, INF/DBG are absent
+            // and Aubio.NET throws an exceptions if a call has failed
+
+            // TODO over time see what's the best approach and fine tune this
+
+            void PrintMessage(ConsoleColor color, string message)
+            {
+                var prev = Console.ForegroundColor;
+                Console.ForegroundColor = color;
+                Console.WriteLine(message);
+                Console.ForegroundColor = prev;
+            }
+
+            Log.SetLevelFunction(LogLevel.Debug,
+                (level, message, data) => { PrintMessage(ConsoleColor.Magenta, message); },
+                IntPtr.Zero);
+
+            Log.SetLevelFunction(LogLevel.Error,
+                (level, message, data) => { PrintMessage(ConsoleColor.Red, message); },
+                IntPtr.Zero);
+
+            Log.SetLevelFunction(LogLevel.Info,
+                (level, message, data) => { PrintMessage(ConsoleColor.Green, message); },
+                IntPtr.Zero);
+
+            Log.SetLevelFunction(LogLevel.Message,
+                (level, message, data) => { PrintMessage(ConsoleColor.Cyan, message); },
+                IntPtr.Zero);
+
+            Log.SetLevelFunction(LogLevel.Warning,
+                (level, message, data) => { PrintMessage(ConsoleColor.Yellow, message); },
+                IntPtr.Zero);
+
+            using (var vec1 = new FVec(20))
+            using (var vec2 = new FVec(10))
+            {
+                Console.WriteLine("Generate an error ...");
+                vec1.Copy(vec2);
+
+                Console.WriteLine("Generate a message ...");
+                vec1.Print();
+            }
+
+            using (var pitch = new Pitch(PitchMethod.Default, 1024, 256, 44100))
+            {
+                try
+                {
+                    Console.WriteLine("Generate a warning ...");
+                    pitch.Silence = 1;
+                }
+                catch (Exception e)
+                {
+                    PrintMessage(ConsoleColor.Red, "Aubio.NET however, will throw if state is invalid !");
+                    Console.WriteLine(e);
+                }
+            }
+
+            Console.WriteLine("Press any key to exit . . .");
+            Console.ReadKey(true);
+        }
+    }
+}
